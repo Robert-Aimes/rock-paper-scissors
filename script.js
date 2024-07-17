ea079b9e5febe4d5c136b24b50a884ae404d59c8
Robert-Aimes <robaimes27@gmail.com>
2024-07-17 16:55:24 -0500

    Working on computer choice logic

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 alert("Hello world!")
+
+function getComputerChoice(){
+    let choice = Math.random;
+    if (choice <=0.33) {
+        return "Rock";
+    } else if ((choice > 0.33) && (choice <= 0.66)) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
